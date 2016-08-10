817ea19444834bf1abf8510664f3ce3a6d880a2a
spinute <spinutids@gmail.com>
2016-08-11 02:03:15 +0900

    implement search bench

--- ext/stbench/bench.rb
+++ ext/stbench/bench.rb
@@ -12,6 +12,17 @@ def bench_insert(keytype, pattern, ht_init_size=0, scale=10, keylen=5)
     Process.waitpid pid
 end
 
+def bench_search(keytype, pattern, ht_init_size=0, scale=10, keylen=5)
+    puts "Search bench: %-15s %-15s %-12s %-15s %s" % ["keytype=#{keytype},", "pattern=#{pattern},", "scale=#{scale},", "ht_init_size=#{ht_init_size},", "keylen=#{keylen}"]
+    pid = Process.fork do
+	bench = STBench.new
+	bench.search_setup keytype, ht_init_size, scale, pattern, keylen
+	puts Benchmark.measure { bench.search_run }
+	bench.search_cleanup
+    end
+    Process.waitpid pid
+end
+
 def bench_init(keytype, ht_init_size=0, scale=1)
     puts "Init bench: %-15s %-12s %s" % ["keytype=#{keytype},", "scale=#{scale}", "ht_init_size=#{ht_init_size}"]
     pid = Process.fork do
@@ -23,6 +34,13 @@ def bench_init(keytype, ht_init_size=0, scale=1)
     Process.waitpid pid
 end
 
+bench_init 'num'
+bench_init 'str'
+4.times do |i|
+    bench_init 'num', 10**i
+    bench_init 'str', 10**i
+end
+
 bench_insert 'num', 'same'
 bench_insert 'num', 'rand'
 bench_insert 'num', 'diff'
@@ -31,9 +49,10 @@ bench_insert 'str', 'rand'
 bench_insert 'str', 'same', 0, 1, 1000
 bench_insert 'str', 'rand', 0, 1, 1000
 
-bench_init 'num'
-bench_init 'str'
-4.times do |i|
-    bench_init 'num', 10**i
-    bench_init 'str', 10**i
-end
+bench_search 'num', 'same'
+bench_search 'num', 'rand'
+bench_search 'num', 'diff'
+bench_search 'str', 'same'
+bench_search 'str', 'rand'
+bench_search 'str', 'same', 0, 1, 1000
+bench_search 'str', 'rand', 0, 1, 1000
--- ext/stbench/stbench.c
+++ ext/stbench/stbench.c
@@ -16,25 +16,25 @@ static st_table *stb_table = NULL;
 #define GET_RAND_CHAR() ('A' + RAND_UPTO(N_OF_CHARS))
 
 static int *int_array = NULL;
-static char **char_array = NULL;
+static char **str_array = NULL;
 
 static void
-fill_array_with_random_strings(int array_size, int str_len) {
+fill_str_array_random(int array_size, int str_len) {
     for (int i = 0; i < array_size; i++) {
 	for (int j = 0; j < str_len; j++)
-	    char_array[i][j] = GET_RAND_CHAR();
-	char_array[i][str_len] = '\0';
+	    str_array[i][j] = GET_RAND_CHAR();
+	str_array[i][str_len] = '\0';
     }
 }
 
 static void
-fill_array_with_random_values(int array_size) {
+fill_int_array_random(int array_size) {
     for (int i = 0; i < array_size; i++)
 	int_array[i] = RAND_UPTO(array_size);
 }
 
 static void
-fill_array_with_different_values(int array_size) {
+fill_int_array_diff(int array_size) {
     /* Fisher-Yates shuffle */
     for (int i = 0; i < array_size; i++)
 	int_array[i] = i;
@@ -56,7 +56,7 @@ typedef enum stbench_scenario_tag
 
     STBenchInit,
     STBenchInsert,
-    STBenchSerch,
+    STBenchSearch,
     STBenchDelete,
 } STBenchScenario;
 static STBenchScenario running_scenario = STBenchNotSet;
@@ -222,6 +222,9 @@ stbench_insert_validate_params(void)
 {
     int acc = 1;
     assert(params_insert.type == KeyTypeNum || params_insert.type == KeyTypeStr);
+    assert(params_insert.pattern == PatternSame ||
+	    params_insert.pattern == PatternDifferent ||
+	    params_insert.pattern == PatternRandom);
     assert(params_insert.n_trial > 0);
     assert(params_insert.ht_init_size >= 0);
     assert(params_insert.key_len > 0);
@@ -282,23 +285,23 @@ stbench_insert_setup(int argc, VALUE argv[], VALUE self)
 	    assert(!int_array);
 	    int_array = xmalloc(sizeof(int) * params_insert.n_trial);
 	    if (params_insert.pattern == PatternSame)
-		fill_array_with_different_values(params_insert.n_trial); /* Dummy for memory usage comparison */
+		fill_int_array_diff(params_insert.n_trial); /* Dummy for memory usage comparison */
 	    else if (params_insert.pattern == PatternDifferent)
-		fill_array_with_different_values(params_insert.n_trial);
+		fill_int_array_diff(params_insert.n_trial);
 	    else if (params_insert.pattern == PatternRandom)
-		fill_array_with_random_values(params_insert.n_trial);
+		fill_int_array_random(params_insert.n_trial);
 	    break;
 	case KeyTypeStr:
-	    assert(!char_array);
-	    char_array = xmalloc(sizeof(char *) * params_insert.n_trial);
+	    assert(!str_array);
+	    str_array = xmalloc(sizeof(char *) * params_insert.n_trial);
 	    for (int i = 0; i < params_insert.n_trial; i++)
-		char_array[i] = xmalloc(sizeof(char) * (params_insert.key_len + 1)); /* +1 for NUL */
+		str_array[i] = xmalloc(sizeof(char) * (params_insert.key_len + 1)); /* +1 for NUL */
 	    if (params_insert.pattern == PatternSame)
-		fill_array_with_random_strings(params_insert.n_trial, params_insert.key_len); /* Dummy for memory usage comparison */
+		fill_str_array_random(params_insert.n_trial, params_insert.key_len); /* Dummy for memory usage comparison */
 	    else if (params_insert.pattern == PatternDifferent)
 		rb_raise(rb_eRuntimeError, "%s: not implemented", __func__);
 	    else if (params_insert.pattern == PatternRandom)
-		fill_array_with_random_strings(params_insert.n_trial, params_insert.key_len);
+		fill_str_array_random(params_insert.n_trial, params_insert.key_len);
 	    break;
 	default:
 	    rb_raise(rb_eRuntimeError, "%s: unexpected key type", __func__);
@@ -327,12 +330,12 @@ stbench_insert_run(VALUE self) {
 	case KeyTypeStr:
 	    if (params_insert.pattern == PatternSame)
 		for (int i = 0; i < params_insert.n_trial; i++)
-		    st_insert(stb_table, (st_data_t) char_array[123], 456);
+		    st_insert(stb_table, (st_data_t) str_array[123], 456);
 	    else if (params_insert.pattern == PatternDifferent)
 		rb_raise(rb_eRuntimeError, "%s: not implemented", __func__);
 	    else if (params_insert.pattern == PatternRandom)
 		for (int i = 0; i < params_insert.n_trial; i++)
-		    st_insert(stb_table, (st_data_t) char_array[i], 456);
+		    st_insert(stb_table, (st_data_t) str_array[i], 456);
 	    break;
 	default:
 	    rb_raise(rb_eRuntimeError, "%s: unexpected key type", __func__);
@@ -359,9 +362,9 @@ stbench_insert_cleanup(VALUE self)
     }
     else if (params_insert.type == KeyTypeStr)
     {
-	assert(char_array);
-	xfree(char_array);
-	char_array = NULL;
+	assert(str_array);
+	xfree(str_array);
+	str_array = NULL;
     }
     else
 	rb_raise(rb_eRuntimeError, "%s: unexpected key type", __func__);
@@ -377,40 +380,175 @@ stbench_insert_cleanup(VALUE self)
 typedef struct params_search_tag
 {
     STBenchKeyType type;
+    STBenchPattern pattern;
     int n_trial;
     int ht_init_size;
     int key_len;
-} ParamsSerch;
-static ParamsSerch params_search;
+} ParamsSearch;
+static ParamsSearch params_search;
 
 static void
 stbench_search_validate_params(void)
 {
     int acc = 1;
     assert(params_search.type == KeyTypeNum || params_search.type == KeyTypeStr);
+    assert(params_search.pattern == PatternSame ||
+	    params_search.pattern == PatternDifferent ||
+	    params_search.pattern == PatternRandom);
     assert(params_search.n_trial > 0);
-    assert(params_search.ht_init_size > 0);
+    assert(params_search.ht_init_size >= 0);
     for (int i = 0; i < params_search.key_len; i++)
     {
 	if (acc > INT_MAX/N_OF_CHARS)
 	    return;
 	acc*=N_OF_CHARS;
     }
-    if (params_insert.n_trial*2 > acc) /* Safety for Different pattern */
+    if (params_search.n_trial*2 > acc) /* Safety for Different pattern */
 	rb_raise(rb_eArgError, "key_len is too short");
 }
 
 static VALUE
 stbench_search_setup(int argc, VALUE argv[], VALUE self) {
+    VALUE arg_type, arg_ht_init_size, arg_scale, arg_pattern, arg_key_len;
+
+    assert(running_scenario == STBenchNotSet);
+    running_scenario = STBenchSearch;
+
+    rb_scan_args(argc, argv, "50", &arg_type, &arg_ht_init_size, &arg_scale, &arg_pattern, &arg_key_len);
+
+    Check_Type(arg_type, T_STRING);
+    Check_Type(arg_pattern, T_STRING);
+
+    params_search = (ParamsSearch) {
+	.type = parse_key_type(rb_string_value_cstr(&arg_type)),
+	.ht_init_size = FIX2INT(arg_ht_init_size),
+	.n_trial = FIX2INT(arg_scale) * SCALE_BASE,
+	.pattern = parse_pattern(rb_string_value_cstr(&arg_pattern)),
+	.key_len = FIX2INT(arg_key_len),
+    };
+
     stbench_search_validate_params();
+
+    switch (params_search.type) {
+	case KeyTypeNum:
+	    if (params_search.ht_init_size > 0)
+		stb_table = st_init_numtable_with_size(params_search.ht_init_size);
+	    else
+		stb_table = st_init_numtable();
+	    break;
+	case KeyTypeStr:
+	    if (params_search.ht_init_size > 0)
+		stb_table = st_init_strtable_with_size(params_search.ht_init_size);
+	    else
+		stb_table = st_init_strtable();
+	    break;
+	default:
+	    rb_raise(rb_eArgError, "%s: unexpected arguments", __func__);
+    }
+
+    switch (params_search.type) {
+	case KeyTypeNum:
+	    assert(!int_array);
+	    int_array = xmalloc(sizeof(int) * params_search.n_trial * 2);
+	    if (params_search.pattern == PatternSame) {
+		fill_int_array_diff(params_search.n_trial);
+		for (int i = 0; i < params_search.n_trial; i++)
+		    st_insert(stb_table, int_array[i], 456);
+		fill_int_array_diff(params_search.n_trial);
+	    }
+	    else if (params_search.pattern == PatternDifferent) {
+		fill_int_array_diff(params_search.n_trial * 2);
+		for (int i = 0; i < params_search.n_trial; i++)
+		    st_insert(stb_table, int_array[params_search.n_trial - 1 - i], 456);
+	    }
+	    else if (params_search.pattern == PatternRandom) {
+		fill_int_array_diff(params_search.n_trial * 2);
+		for (int i = 0; i < params_search.n_trial; i++)
+		    st_insert(stb_table, int_array[i], 456);
+		fill_int_array_diff(params_search.n_trial * 2);
+	    }
+	    break;
+	case KeyTypeStr:
+	    assert(!str_array);
+	    str_array = xmalloc(sizeof(char *) * params_search.n_trial * 2);
+	    for (int i = 0; i < params_search.n_trial * 2; i++)
+		str_array[i] = xmalloc(sizeof(char) * (params_search.key_len + 1)); /* +1 for NUL */
+
+	    /* TODO: fake implementation, have to implement different */
+	    fprintf(stderr, "XXX: fake implementation, have to implement fill diff.\n");
+	    if (params_search.pattern == PatternSame) {
+		fill_str_array_random(params_search.n_trial, params_search.key_len);
+		for (int i = 0; i < params_search.n_trial; i++)
+		    st_insert(stb_table, (st_data_t)str_array[i], 456);
+		//fill_str_array_random(params_search.n_trial, params_search.key_len);
+	    }
+	    else if (params_search.pattern == PatternDifferent) {
+		/* TODO: fake implementation, have to implement different */
+		fill_str_array_random(params_search.n_trial * 2, params_search.key_len);
+		for (int i = 0; i < params_search.n_trial; i++)
+		    st_insert(stb_table, (st_data_t)str_array[params_search.n_trial - 1 - i], 456);
+	    }
+	    else if (params_search.pattern == PatternRandom) {
+		fill_str_array_random(params_search.n_trial * 2, params_search.key_len);
+		for (int i = 0; i < params_search.n_trial; i++)
+		    st_insert(stb_table, (st_data_t)str_array[i], 456);
+		fill_str_array_random(params_search.n_trial * 2, params_search.key_len);
+	    }
+	    break;
+	default:
+	    rb_raise(rb_eRuntimeError, "%s: unexpected key type", __func__);
+	    break;
+    }
+
+    rss_before = get_rss();
+
     return self;
 }
 static VALUE
 stbench_search_run(VALUE self) {
+    st_data_t ret_value;
+    switch (params_search.type) {
+	case KeyTypeNum:
+	    for (int i = 0; i < params_search.n_trial; i++)
+		st_lookup(stb_table, int_array[i], &ret_value);
+	    break;
+	case KeyTypeStr:
+	    for (int i = 0; i < params_search.n_trial; i++)
+		st_lookup(stb_table, (st_data_t)str_array[i], &ret_value);
+	    break;
+	default:
+	    rb_raise(rb_eRuntimeError, "%s: unexpected key type", __func__);
+	    break;
+    }
+
     return self;
 }
 static VALUE
 stbench_search_cleanup(VALUE self) {
+    rss_after = get_rss();
+    REPORT_RSS();
+
+    assert(running_scenario == STBenchSearch);
+    running_scenario = STBenchNotSet;
+
+    if (params_search.type == KeyTypeNum)
+    {
+	assert(int_array);
+	xfree(int_array);
+	int_array = NULL;
+    }
+    else if (params_search.type == KeyTypeStr)
+    {
+	assert(str_array);
+	xfree(str_array);
+	str_array = NULL;
+    }
+    else
+	rb_raise(rb_eRuntimeError, "%s: unexpected key type", __func__);
+
+    assert(stb_table);
+    st_free_table(stb_table);
+    stb_table = NULL;
     return self;
 }
 
@@ -428,7 +566,7 @@ stbench_delete_validate_params(void)
 {
     assert(params_delete.type == KeyTypeNum || params_delete.type == KeyTypeStr);
     assert(params_delete.n_trial > 0);
-    assert(params_delete.ht_init_size > 0);
+    assert(params_delete.ht_init_size >= 0);
 }
 
 static VALUE
